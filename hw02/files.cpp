53bc3d43a37d64af203eb0007c2f8a9bdb497a07
rjlawton <rjlawton@alaska.edu>
2019-09-27 00:51:38 -0800

    Getting started

---
+++ hw02/files.cpp
@@ -0,0 +1,18 @@
+// Homework #2 files.hpp
+// Robert Lawton
+// CS 202 with Hartman
+// 9/26/2019
+
+#include "files.hpp"
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ostream>
+#include <sstream>
+
+
+
+void getIDFromFile(std::string file, std::istream input, std::ostream  out){
+
+}
---
+++ hw02/files.hpp
@@ -0,0 +1,13 @@
+// Homework #2 files.hpp
+// Robert Lawton
+// CS 202 with Hartman
+// 9/26/2019
+
+#ifndef FILES_HPP
+#define FILES_HPP
+
+
+
+
+
+#endif
