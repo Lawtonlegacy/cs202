d3a7a87dabb3833ade60a4d191ad75f622e1e322
rjlawton <rjlawton@alaska.edu>
2019-10-11 05:56:06 -0800

    Getting started. Added my code from original box class

---
+++ hw03/box.cpp
@@ -0,0 +1,71 @@
+// Homework #3 box.cpp
+// Robert Lawton
+// CS 202 with Hartman
+// 10/11/2019
+
+#include "box.hpp"
+
+
+//Default Constructor
+Box::Box() :
+	_height(1), _width(1), _filled(true) {}
+
+//Constructor for user dimension input
+Box::Box(int width, int height) :
+	_height(height), _width(width), _filled(true) {}
+
+//Constructor to determine whether filled or hollow and dimension input
+Box::Box(int width, int height, bool filled) :
+	_height(height), _width(width), _filled(filled) {}
+
+//Member funciton named type() that returns string "Filled" or string "Hollow
+std::string Box::type() const {
+	if (_filled) {
+		return "Filled";
+	}
+	return "Hollow";
+}
+
+//getWidth() accessor function
+int Box::getWidth() const {
+	return _width;
+}
+
+//getHeight() accessor function
+int Box::getHeight() const {
+	return _height;
+}
+
+//setWidth() mutator function
+void Box::setWidth(const int& width) {
+	_width = width;
+}
+
+//setHeight() mutator function
+void Box::setHeight(const int& height) {
+	_height = height;
+}
+
+//Member function that draws the box to the ostream
+void Box::print(std::ostream& output) const {
+
+	for (int row = 0; row < _height; ++row) {
+
+		for (int col = 0; col < _width; col++) {
+
+			if (row == 0 || row == _height - 1) {
+				output << "x";
+			}
+			else if (col == 0 || col == _width - 1) {
+				output << "x";
+			}
+			else if (_filled) {
+				output << "x";
+			}
+			else {
+				output << " ";
+			}
+		}
+		output << "\n";
+	}
+}
---
+++ hw03/box.hpp
@@ -0,0 +1,43 @@
+// Homework #3 box.cpp
+// Robert Lawton
+// CS 202 with Hartman
+// 10/11/2019
+
+#ifndef BOX_HPP
+#define BOX_HPP
+
+#include <string>
+#include <ostream>
+
+
+class Box {
+
+public:
+
+	Box();
+
+	Box(int width, int height);
+
+	Box(int width, int height, bool filled);
+
+	std::string type() const;
+
+	int getWidth() const;
+
+	int getHeight() const;
+
+	void setWidth(const int& width);
+
+	void setHeight(const int& height);
+
+	void print(std::ostream& stream) const;
+
+private:
+
+	int _height;
+	int _width;
+	bool _filled;
+
+};
+
+#endif 
