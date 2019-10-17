b12ce5a92ae3666b7a4b8990a917618fbc29f729
Robert Lawton <rjlawton@alaska.edu>
2019-10-16 17:28:54 -0800

    Added Canonical functions from AKQuaternion

--- hw04/money.cpp
+++ hw04/money.cpp
@@ -3,3 +3,32 @@
 // CS 202 with Hartman
 // 10/16/2019
 
+#include "money.hpp"
+
+
+
+
+
+
+
+//**************Canonical Functions from Hartman****************
+Money& Money::operator+=(const Money& rhs) {
+	return *this = *this + rhs;
+}
+
+Money& Money::operator-=(const Money& rhs) {
+	return *this = *this - rhs;
+}
+
+Money operator*(Money lhs, const Money& rhs) { return lhs *= rhs; }
+
+
+Money operator/(Money lhs, const Money& rhs) { return lhs /= rhs; }
+
+
+bool operator>(const Money& lhs, const Money& rhs) { return rhs < lhs; }
+
+
+bool operator!=(const Money& lhs, const Money& rhs) {
+	return !(lhs == rhs);
+}
--- hw04/money.hpp
+++ hw04/money.hpp
@@ -1,5 +1,29 @@
-// Homework #4 money.hpp
+// Homework #4 money.cpp
 // Robert Lawton
 // CS 202 with Hartman
 // 10/16/2019
+
+#include <iostream>
+
+class Money {
+
+private:
+
+	double _balance;
+
+public:
+
+	Money& operator+=(const Money& rhs);
+
+	Money& operator-=(const Money& rhs);
+
+};
+
+Money operator*(Money lhs, const Money& rhs);
+
+Money operator/(Money lhs, const Money& rhs);
+
+bool operator>(const Money& lhs, const Money& rhs);
+
+bool operator!=(const Money& lhs, const Money& rhs);
 
