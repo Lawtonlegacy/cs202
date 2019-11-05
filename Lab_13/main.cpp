43fd7012cf3f83cc2b988f67f057694f64047157
rjlawton <rjlawton@alaska.edu>
2019-11-04 16:31:36 -0900

    Might be done

---
+++ Lab_13/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+template<typename T>
+T twice(const T& x) {
+	return x + x;
+}
+
+std::string twice(const char* ptr) {
+	std::string s(ptr);
+	return s + s;
+}
+
+
+int main() {
+
+	//Part 1
+	std::cout << twice(2) << std::endl;
+	std::cout << twice(2.3) << std::endl;
+	std::cout << twice(std::string("Hello")) << std::endl;
+
+	//Part 2 & 3 w/ C-style string
+	const char *cstring = "World";
+	std::cout << twice(cstring) << std::endl;
+
+	return 0;
+}
