8b3df7c150713e0ffc406fc5d879ff46b3df1e6d
rjlawton <rjlawton@CEM-SOECAL-30>
2019-11-19 10:38:50 -0900

    Getting Started

---
+++ Lab_15/main.cpp
@@ -0,0 +1,11 @@
+#include "wrapper.hpp"
+
+
+
+int main() {
+
+
+
+
+	return 0;
+}
---
+++ Lab_15/wrapper.hpp
@@ -0,0 +1,17 @@
+#ifndef WRAPPER_HPP
+#define WRAPPER_HPP
+
+template <typename T>
+class Wrapper {
+
+public:
+	Wrapper(const T& obj);
+
+private:
+	T _first
+};
+
+
+
+
+#endif;
