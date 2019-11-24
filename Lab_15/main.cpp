7ce733e215b82d6d084c64625b5b9dad8ab7ac36
Robert Lawton <rjlawton@alaska.edu>
2019-11-24 12:42:20 -0900

    Finished

--- Lab_15/main.cpp
+++ Lab_15/main.cpp
@@ -4,8 +4,10 @@
 
 int main() {
 
+	Wrapper<int>w{ 2 };
+	Wrapper<std::string> s{ "Hello world!" };
 
-
+	std::cout << w << " " << s << std::endl;
 
 	return 0;
 }
--- Lab_15/wrapper.hpp
+++ Lab_15/wrapper.hpp
@@ -1,17 +1,23 @@
 #ifndef WRAPPER_HPP
 #define WRAPPER_HPP
+#include <iostream>
 
 template <typename T>
 class Wrapper {
-
+	template<typename U>
+	friend std::ostream& operator<<(std::ostream& os, const Wrapper<U>& type);
 public:
-	Wrapper(const T& obj);
+	Wrapper(const T& obj) :_first(obj) {}
 
 private:
-	T _first
+	T _first;
 };
 
+template<typename U>
+std::ostream& operator<<(std::ostream& os, const Wrapper<U>& type) {
+	os << type._first;
+	return os;
+}
 
 
-
-#endif;
+#endif
