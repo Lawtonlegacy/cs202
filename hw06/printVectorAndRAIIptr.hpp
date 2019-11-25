22ca6a7860c58d395cad20eeb3ebdfff2e38b0d1
Robert Lawton <rjlawton@alaska.edu>
2019-11-24 15:11:43 -0900

    Should be finished..

--- hw06/printVectorAndRAIIptr.hpp
+++ hw06/printVectorAndRAIIptr.hpp
@@ -5,8 +5,8 @@
 #include <vector>
 
 //Part A printVector template function
-template<typename V>
-void printVector(std::ostream& os, std::vector <V> any) {
+template<typename T>
+void printVector(std::ostream& os, std::vector <T> any) {
 	for (auto i : any) {
 		os << i << std::endl;
 	}
@@ -17,22 +17,38 @@ void printVector(std::ostream& os, std::vector <V> any) {
 template<typename T>
 class RAIIPtr {
 public:
-	RAIIPtr(const T* &ptr);
+
+	//Constructor
+	RAIIPtr(T* ptr);
 	
 	//Overloaded operator* (dereference)
-	T& operator*(T ptr) {
-		return *ptr;
-	};
+	T& operator*();
 
 	//Overloaded operator->
-	T* operator->;
+	T operator->();
+
+	//Destuctor
+	~RAIIPtr();
+
 private:
-	T _ptrVal;
+	T* _raiiPtr;
 };
 
 //Constructor which takes a T*
 template <typename T>
-RAIIPtr<T>::RAIIPtr(const T*& ptr) :_ptrVal(ptr) {}
+RAIIPtr<T>::RAIIPtr(T* ptr) :_raiiPtr(ptr) {}
+
+//Destrutor
+template <typename T>
+RAIIPtr<T>::~RAIIPtr() { delete(_raiiPtr); }
+
+//Overloaded operator*
+template <typename T>
+T& RAIIPtr<T>::operator*() { return _raiiPtr; };
+
+//Overloaded operator->
+template <typename T>
+T RAIIPtr<T>::operator->() { return _raiiPtr; };
 
 
 #endif
